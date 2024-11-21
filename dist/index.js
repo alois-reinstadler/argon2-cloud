83c4696962b3509449421c93a5881df9408d6ec1
Alois R <alois-reinstadler@proton.me>
2024-11-21 16:10:47 +0100

    handle argon2 error

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,58 @@
 import { Hono } from 'hono';
+import { cors } from 'hono/cors';
+import { validator } from 'hono/validator';
 import { serve } from '@hono/node-server';
+import { hash, verify } from '@node-rs/argon2';
 const app = new Hono();
 const port = parseInt(`${process.env.PORT || 8080}`);
-app.get("/", (c) => {
-    return c.text("Hello Hono!");
+const argonOptions = {
+    // recommended minimum parameters
+    memoryCost: 19456, // ~ 20MB
+    timeCost: 2,
+    outputLen: 32,
+    parallelism: 1,
+};
+const api = app.use("/api", cors({ origin: ["https://alrein.dev", "https://postkorb.app"] }));
+api.post("/hash", validator("form", (value, c) => {
+    const password = value["password"];
+    if (!password ||
+        typeof password !== "string" ||
+        password.length < 8 ||
+        password.length > 64) {
+        return c.text("Bad Request", 400);
+    }
+    return { password };
+}), async (c) => {
+    const { password } = c.req.valid("form");
+    const hashed = await hash(password, argonOptions);
+    return c.json({
+        hash: hashed,
+    });
 });
-console.log(`App listening on port: ${port}`);
+api.post("/verify", validator("form", (value, c) => {
+    const password = value["password"];
+    const hash = value["hash"];
+    if (!validatePassword(password) || !validateHash(hash)) {
+        return c.text("Bad Request", 400);
+    }
+    return { password, hash };
+}), async (c) => {
+    const { password, hash } = c.req.valid("form");
+    const matches = await verify(hash, password, argonOptions);
+    return c.json({ matches });
+});
 serve({
     fetch: app.fetch,
     port,
 });
+// utils
+function validatePassword(value) {
+    if (Array.isArray(value))
+        value = value[0];
+    return Boolean(value && typeof value === "string" && value.length > 8 && value.length <= 64);
+}
+function validateHash(value) {
+    if (Array.isArray(value))
+        value = value[0];
+    return Boolean(value && typeof value === "string" && value.length > 4);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,127 @@
 import { Hono } from 'hono';
+import { cors } from 'hono/cors';
+import { validator } from 'hono/validator';
 
 import { serve } from '@hono/node-server';
+import { hash, verify } from '@node-rs/argon2';
+
+import type { ParsedFormValue } from "hono/types";
+import type { Options as Argon2Options } from "@node-rs/argon2";
 
 const app = new Hono();
 const port = parseInt(`${process.env.PORT || 8080}`);
 
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
-});
+const argonOptions = {
+  // recommended minimum parameters
+  memoryCost: 19456, // ~ 20MB
+  timeCost: 2,
+  outputLen: 32,
+  parallelism: 1,
+} satisfies Argon2Options;
 
-console.log(`App listening on port: ${port}`);
+app.use(
+  "/*",
+  cors()
+  // cors({ origin: ["https://alrein.dev", "https://postkorb.app"] })
+);
+
+app.post(
+  "/hash",
+  validator("form", (value, c) => {
+    const password = value["password"];
+
+    if (!validatePassword(password)) {
+      return c.text("Bad Request", 400);
+    }
+
+    return { password };
+  }),
+  async (c) => {
+    const { password } = c.req.valid("form");
+    const hashed = await hash(password, argonOptions);
+
+    return c.json({
+      hash: hashed,
+    });
+  }
+);
+
+app.post(
+  "/hash",
+  validator("form", (value, c) => {
+    const password = value["password"];
+
+    if (!validatePassword(password)) {
+      return c.text("Bad Request", 400);
+    }
+
+    return { password };
+  }),
+  async (c) => {
+    const { password } = c.req.valid("form");
+
+    try {
+      const hashed = await hash(password, argonOptions);
+      return c.json({ hash: hashed });
+    } catch (error) {
+      return c.text("Bad Request", 400);
+    }
+  }
+);
+
+app.post(
+  "/verify",
+  validator("form", (value, c) => {
+    const password = value["password"];
+    const hashed = value["hash"];
+
+    if (!validatePassword(password) || !validateHash(hashed)) {
+      return c.text("Bad Request", 400);
+    }
+
+    return { password, hashed };
+  }),
+  async (c) => {
+    const { password, hashed } = c.req.valid("form");
+
+    try {
+      const matches = await verify(hashed, password, argonOptions);
+      return c.json({ matches });
+    } catch (error) {
+      if (isArgon2Error(error)) return c.json({ matches: false });
+      return c.text("Bad Request", 400);
+    }
+  }
+);
 
 serve({
   fetch: app.fetch,
   port,
 });
+
+// utils
+function validatePassword(
+  value: ParsedFormValue | ParsedFormValue[]
+): value is string {
+  if (Array.isArray(value)) value = value[0];
+  return Boolean(
+    value && typeof value === "string" && value.length > 8 && value.length <= 64
+  );
+}
+
+function validateHash(
+  value: ParsedFormValue | ParsedFormValue[]
+): value is string {
+  if (Array.isArray(value)) value = value[0];
+  return Boolean(value && typeof value === "string" && value.length > 4);
+}
+
+function isArgon2Error(error: unknown): error is Argon2Error {
+  return (
+    error instanceof Error && "code" in error && error.code === "InvalidArg"
+  );
+}
+
+interface Argon2Error extends Error {
+  code: string;
+}
